2a4ca965172690be2946060510a35014873e3a3c
Heesho <100749457+Heesho@users.noreply.github.com>
2024-01-28 12:27:29 -0800

    first commit

--- src/meme-factory.ts
+++ src/meme-factory.ts
@@ -1,57 +1,71 @@
 import {
   MemeFactory__MemeCreated as MemeFactory__MemeCreatedEvent,
   MemeFactory__TreasuryUpdated as MemeFactory__TreasuryUpdatedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/MemeFactory/MemeFactory"
-import {
-  MemeFactory__MemeCreated,
-  MemeFactory__TreasuryUpdated,
-  OwnershipTransferred
-} from "../generated/schema"
+  OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/MemeFactory/MemeFactory";
+import { Directory } from "../generated/schema";
+import { BigInt, Address } from "@graphprotocol/graph-ts";
+import { MemeFactory as FactoryContract } from "../generated/MemeFactory/MemeFactory";
+import { FACTORY_ADDRESS } from "./helpers";
 
 export function handleMemeFactory__MemeCreated(
   event: MemeFactory__MemeCreatedEvent
 ): void {
-  let entity = new MemeFactory__MemeCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.index = event.params.index
-  entity.meme = event.params.meme
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let factory = Directory.load(FACTORY_ADDRESS);
+  if (factory === null) {
+    factory = new Directory(FACTORY_ADDRESS);
+    factory.memeCount = 0;
+    factory.transactionCount = BigInt.fromI32(0);
+    factory.totalVolume = BigInt.fromI32(0);
+    factory.totalLiquidity = BigInt.fromI32(0);
+    let factoryContract = FactoryContract.bind(
+      Address.fromString(FACTORY_ADDRESS)
+    );
+    factory.owner = factoryContract.owner();
+    factory.treasury = factoryContract.treasury();
+  }
+  factory.memeCount = factory.memeCount + 1;
+  factory.save();
 }
 
 export function handleMemeFactory__TreasuryUpdated(
   event: MemeFactory__TreasuryUpdatedEvent
 ): void {
-  let entity = new MemeFactory__TreasuryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.treasury = event.params.treasury
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let factory = Directory.load(FACTORY_ADDRESS);
+  let factoryContract = FactoryContract.bind(
+    Address.fromString(FACTORY_ADDRESS)
+  );
+  if (factory === null) {
+    factory = new Directory(FACTORY_ADDRESS);
+    factory.memeCount = 0;
+    factory.transactionCount = BigInt.fromI32(0);
+    factory.totalVolume = BigInt.fromI32(0);
+    factory.totalLiquidity = BigInt.fromI32(0);
+    factory.owner = factoryContract.owner();
+    factory.treasury = factoryContract.treasury();
+  } else {
+    factory.treasury = factoryContract.treasury();
+  }
+  factory.save();
 }
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let factory = Directory.load(FACTORY_ADDRESS);
+  let factoryContract = FactoryContract.bind(
+    Address.fromString(FACTORY_ADDRESS)
+  );
+  if (factory === null) {
+    factory = new Directory(FACTORY_ADDRESS);
+    factory.memeCount = 0;
+    factory.transactionCount = BigInt.fromI32(0);
+    factory.totalVolume = BigInt.fromI32(0);
+    factory.totalLiquidity = BigInt.fromI32(0);
+    factory.owner = factoryContract.owner();
+    factory.treasury = factoryContract.treasury();
+  } else {
+    factory.owner = factoryContract.treasury();
+  }
+  factory.save();
 }
